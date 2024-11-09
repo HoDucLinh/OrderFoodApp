6f58308f8bf9fae47f26dddccb66d4a28a84eb63
thien0709 <thien070904@gmail.com>
2024-11-09 15:27:23 +0700

    update change theme ,margin layout ,paymentnomc

--- app/src/main/java/com/ltb/orderfoodapp/view/Welcome.kt
+++ app/src/main/java/com/ltb/orderfoodapp/view/Welcome.kt
@@ -1,9 +1,12 @@
 package com.ltb.orderfoodapp.view
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 import com.ltb.orderfoodapp.R
@@ -21,4 +24,15 @@ class Welcome : AppCompatActivity() {
             finish()
         }
     }
+    override fun onStart() {
+        super.onStart()
+        val sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val nightMode = sharedPreferences.getBoolean("night", false)
+        if (nightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+    }
+
 }
